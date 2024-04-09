45f0f57a72f7d865e18fb6869f4b71c96faba0d7
Meller95 <140019960+Meller95@users.noreply.github.com>
2024-04-09 09:29:26 +0200

    test

---
+++ EmployeesV10/EmployeesV10/ITSupporter.cs
@@ -0,0 +1,25 @@
+﻿
+using EmployeesV10;
+
+public class ITSupporter : Employee
+{
+    #region Properties
+    public string PrimaryWorkArea { get; set; }
+
+    public string AllInformation
+    {
+        get
+        {
+            return $"IT-Supporter {Name} works {HoursPerWeek} hours/week, mostly with {PrimaryWorkArea}";
+        }
+    }
+    #endregion
+
+    #region Constructor
+    public ITSupporter(string name, int hoursPerWeek, string primaryWorkArea) : base(name, hoursPerWeek)
+    {
+        
+        PrimaryWorkArea = primaryWorkArea;
+    }
+    #endregion
+}
---
+++ MovieManagerV10/MovieManagerV10/Program.cs
@@ -0,0 +1,26 @@
+﻿
+Movie movieA = new Movie("Alien", "Ridley Scott", 112);
+Movie movieB = new Movie("Inception", "Christopher Nolan", 162);
+Movie movieC = new Movie("The Matrix", "The Wachowskis", 136);
+Movie movieD = new Movie("The Shawshank Redemption", "Frank Darabont", 142);
+
+Console.WriteLine("Before calls of Watch():");
+Console.WriteLine($"{movieA.Title}, by {movieA.Director}, " +
+                  $"watched it {movieA.NoOfViews} time(s)");
+Console.WriteLine($"{movieB.Title}, by {movieB.Director}, " +
+                  $"watched it {movieB.NoOfViews} time(s)");
+Console.WriteLine($"{movieC.Title}, by {movieC.Director}, " +
+                  $"watched it {movieC.NoOfViews}");
+
+movieA.Watch();
+movieA.Watch();
+movieB.Watch();
+movieC.Watch();
+
+Console.WriteLine("After calls of Watch():");
+Console.WriteLine($"{movieA.Title}, by {movieA.Director}, " +
+                  $"watched it {movieA.NoOfViews} time(s)");
+Console.WriteLine($"{movieB.Title}, by {movieB.Director}, " +
+                  $"watched it {movieB.NoOfViews} time(s)");
+Console.WriteLine($"{movieC.Title}, by {movieC.Director}, " +
+                  $"watched it {movieB.NoOfViews} time(s)");
