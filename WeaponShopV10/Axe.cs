dfc35e8678d6436b7403402ec18f660f1f8f88e9
Meller95 <140019960+Meller95@users.noreply.github.com>
2024-04-09 11:04:01 +0200

    sd

---
+++ WeaponShopV10/Axe.cs
@@ -0,0 +1,31 @@
+﻿
+/// <summary>
+/// This class represents a Axe. An Axe is 
+/// considered to be a weapon.
+/// </summary>
+public class Axe : Weapon
+{
+    public const int InitialAxeMinDamage = 20;
+    public const int InitialAxeMaxDamage = 50;
+
+    public double DamageFromAxe()
+    {
+        double damage = CalculateDamage();
+        MinDamage -= 3;
+        MaxDamage -= 3;
+        return damage;
+
+    }
+
+    public void Sharpen()
+    {
+        MinDamage = InitialAxeMinDamage;
+        MaxDamage = InitialAxeMaxDamage;
+    }
+    #region Constructor
+    public Axe(string description)
+        : base(description, InitialAxeMinDamage, InitialAxeMaxDamage)
+    {
+    }
+    #endregion
+}
