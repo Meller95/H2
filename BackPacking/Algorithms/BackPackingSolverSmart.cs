7eceb4324b861a6e914f5993b5355b36a329a018
Meller95 <140019960+Meller95@users.noreply.github.com>
2024-04-12 10:08:10 +0200

    BackPacking Løst

---
+++ BackPacking/Algorithms/BackPackingSolverSmart.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackPacking.Algorithms
+{
+    public class BackPackingSolverSmart : BackPackingSolverBase
+{
+    public BackPackingSolverSmart(List<BackPackItem> items, double capacity) : base(items, capacity) { }
+
+        public override void Solve(ItemVault theItemVault, BackPack theBackPack)
+        {
+            while (true)
+            {
+                var bestItem = PickNextItemByValueToWeightRatio(theItemVault, theBackPack.WeightCapacityLeft);
+                if (bestItem == null)
+                    break;
+
+                theBackPack.AddItem(bestItem);
+                theItemVault.RemoveItem(bestItem.Description);
+            }
+        }
+
+    }
+
+}
